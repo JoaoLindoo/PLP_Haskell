4c45e8de865e45a288c6023d01fdc15d057f992e
jao <joao.soares@ccc.ufcg.edu.br>
2018-06-20 01:34:54 -0300

    add outra funcao

--- bomber.hs
+++ bomber.hs
@@ -23,9 +23,20 @@ tela_principal = do
 	cont <- readFile ".msg"
 	showLines (take 21 (lines cont))
 
+entrada :: IO()
+entrada = do
+    a <- getLine
+    putStrLn ("oi " ++ a)
+    
 main :: IO()
 main = do
     tela_principal
+    putStrLn "Bem Vindo ao ano começo do fim , a gasolina está acabando e o mundo também só você poderá salva-lo e deter o virus mortal"
+    putStrLn "Baseado em uma historia real. Se voce nao lembra foi por que o mundo ja foi salvo e sua linha do tempo mudou"
     putStrLn "[1] Pronto para a simulação :"
+    a <- getLine
     
+    if(a == "1")
+        then putStrLn "Inicia o jogo"
+        else putStrLn "FIM DO MUNDO"
    

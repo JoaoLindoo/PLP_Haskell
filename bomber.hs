d8bef7756ca1120859847d6455a72926659ea347
jao <joao.soares@ccc.ufcg.edu.br>
2018-06-20 00:50:24 -0300

    Modificacoes

--- bomber.hs
+++ bomber.hs
@@ -1,19 +1,25 @@
-main :: IO()
-main = do
-    putStrLn "\nBem Vindo ao ano começo do fim , a gasolina está acabando e o mundo também só você poderá salva-lo e deter o virus mortal"
-    putStrLn "Baseado em uma historia real. Se voce nao lembra foi por que o mundo ja foi salvo e sua linha do tempo mudou\n"
-    name <- getLine
-    putStrLn("Hey " ++ name ++ " ,you rock!")
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
 
-menu_grafico :: IO() 
-menu_grafico = do
-    putStrLn "No ano de 2046, um grupo de cientista conseguem terminar o maior feito da humanidade “um botão de reiniciar” uma máquina do tempo. Motivo para ser comemorado pois a única salvação da terra é voltar no tempo e destruir todas as bases que contém o vírus que acabou com quase toda a população do planeta. Irineu foi o soldado qualificado para essa missão , voltando para o ano de 2014, após 5 anos preso nas instalações secretas da CIA conseguiu convencer o diretor da CIA alertando de diversos desastres antes mesmo de ter acontecido. Só que a arma biológica irá ser usada no ano de 2019 no dia 5 de março e faltando apenas um dia para impedir esses ataques , vão bombardear pontos exatos para a destruição dessas instalações e assim salvando a humanidade. \n Você deve escolher uma posição dada pelas letras e pelos numeros de 0 a 11. Se acertar uma base é retornado o número da base, se não será retornado um X. O jogo acaba quando a base de todas as bases do inimigo forem destruidas "
+{- --------------------------------------------
+   Funções de interação com terminal
+   ---------------------------------------------}
 
-desenhar_interface :: IO()
-desenhar_interface = do
-        putStrLn"####################################################"
-        putStrLn"########      bombardment of the virus      ########"
-        putStrLn"####################################################"
-        putStrLn"     A   B   C   D   E   F   G   H   I   J   L   M"
-        putStrLn"----------------------------------------------------"
-		
+{-Comando para dar scape no terminal-}
+cleanScreen :: IO()
+cleanScreen = putStr "\ESC[2J"
+
+{-Imprime as strings de uma lista de string-}
+showLines :: [String] -> IO()
+showLines [] = return ()
+showLines s = do 
+	putStrLn (head s)
+	showLines (tail s)
+
+{-Exibe mensagem inicial do jogo-}
+showEnterDialog :: IO ()
+showEnterDialog = do
+	cont <- readFile ".msg"
+	showLines (take 21 (lines cont))
+
+

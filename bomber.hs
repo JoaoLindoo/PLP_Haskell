9d0bcc5be000677760b774382553c57d7583d721
matheusthiago95 <matheus.silva@ccc.ufcg.edu.br>
2018-06-22 01:50:41 -0300

    Algumas alterações

--- bomber.hs
+++ bomber.hs
@@ -1,20 +1,35 @@
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
-
+import Data.Array
+import System.Random
 -- Author joao.soares
 {- --------------------------------------------
    Funções de interação com terminal
    ---------------------------------------------}
 {- Representa um Jogador com o seu tabuleiro -}
-data Jogador =
-    Jogador
-    Tabuleiro
-    Bombas
-{-Tabuleiro-}
-type Tabuleiro = Map (Int,Int) Char
-{- Bombas de lançamento -}
-type Bombas = Int
+data Jogador = Jogador {tabuleiro :: Array (Int,Int) Int, bombas :: Int} deriving(Show)
 
+
+{-Metodo que cria a matriz base de inteiro com valor zero-}
+criarMatrizZero :: Int -> Int -> Array (Int,Int) Int
+criarMatrizZero x n = array ((1,1),(x,n)) [((i,j),0)|i<-[1..x],j<-[1..n]]
+
+{-Metodo que cria a objeto jogador com tabuleiro 12X12-}
+criarJogador :: Int -> Jogador
+criarJogador nBombas = Jogador (criarMatrizZero 12 12) nBombas 
+
+{-Variavel que representar o jogado com tabuleiro vazio (apenas zero) e com 45 bombas para se jogar-}
+jogador_base = criarJogador 45
+
+{-atualiza o valor no tabuleiro do jogador (atualizacao feita a partir de uma soma), informando o numero da linha e coluna-}
+atualizarTabuleiro :: Jogador -> Int -> Int -> Int -> Jogador
+atualizarTabuleiro jogador linha coluna valor = Jogador (accum (+) (tabuleiro jogador) [((linha,coluna), valor)]) (bombas jogador)
+
+{-Gera um numero Random mas a saida é um Inteiro IO-}
+numeroRan :: Int -> Int -> IO Int
+numeroRan v1 v2 = do
+	nR <- randomRIO(v1, v2::Int)
+	return (nR)
 {-Comando para dar scape no terminal-}
 cleanScreen :: IO()
 cleanScreen = putStr "\ESC[2J"

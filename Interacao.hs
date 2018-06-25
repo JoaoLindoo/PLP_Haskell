8f54a0fd42b033aeb54af2a1bffcdda7f54e1514
João Henrique <joao.soares@ccc.ufcg.edu.br>
2018-06-25 20:25:05 -0300

    Update Interacao.hs

--- Interacao.hs
+++ Interacao.hs
@@ -35,32 +35,3 @@ showVitoria :: IO()
 showVitoria = do
 	cont <- readFile ".msg"
 	showLines (take 40 (drop 38 (lines cont)))
-
-{-Retorna a string com a visão do usuário do
-  tabuleiro-}
-showTabuleiro :: Map (Int,Int) Char -> (Int,Int) -> String
-showTabuleiro m t@(i,j)
-	{-Imprimindo as bordas esquerdas-}
-	| j == 0    = if (i < 11)
-					then 
-						(show2Dig (i-1)) ++ " │ " ++ (showTabuleiro m (i,j+1))
-						else
-							""
-	{-Imprimindo as bodas direitas-}
-	| j == 11   = if (i > 10)
-					then " │\n"
-					else (" │\n" ++ showTabuleiro m (i+1,0))
-	{-Imprimindo os tiros-}
-	| otherwise = if (Map.notMember t m)
-					{-Imprimindo água-}
-					then (terreno: showTabuleiro m (i,j+1))
-					else do
-						if (m ! (i,j) == base)
-						 {-Escondendo bases -}
-						 then (terreno : showTabuleiro m (i,j+1))
-						 else if (m ! (i,j) == wShot)
-						 	{-Tiro perdido-}
-						 	then (wShot : " " ++ showTabuleiro m (i,j+1))
-						 	{-Tiro realizado na base-}
-						 	else (sShot : " " ++ showTabuleiro m (i,j+1))
-
